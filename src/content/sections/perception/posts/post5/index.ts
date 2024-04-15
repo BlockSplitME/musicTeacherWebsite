43305f3223bb34f7d6eead23350267cb43b362ea
BlockSplitME <evgeniy.belov.12@mail.ru>
2024-04-15 21:25:53 +0300

    update

---
+++ src/content/sections/perception/posts/post5/index.ts
@@ -0,0 +1,6 @@
+import { CardPostType } from '@/entities/post/types';
+
+export default {
+    title: 'Г.Свиридов «Военный марш»',
+    subtitle: 'Это марш бодрый, смелый, бесстрашный, в нём чувствуется радостное, праздничное настроение...',
+} as CardPostType
