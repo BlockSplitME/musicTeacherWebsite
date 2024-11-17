34b95818b7c3e7463186d06a530bd6e77a76ce94
BlockSplitME <evgeniy.belov.12@mail.ru>
2024-11-17 19:21:44 +0300

    Добавлен пост танцующие снежинки

---
+++ src/content/sections/singing/posts/post2/Post.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="auido__block_center">
+        <audio controls>
+            <source src="./audio/А.Филипенко - «Танец Снежинок».mp3" type="audio/mpeg">
+        </audio>
+    </div>
+    <ImageBlock>
+        <img alt="танец_снежинок" src="./image/танец_снежинок.jpg">
+    </ImageBlock>
+</template>
+<style scoped>
+.auido__block_center {
+    display: flex;
+    justify-content: center;
+    padding-top: 16px;
+}
+</style>
---
+++ src/content/sections/singing/posts/post2/index.ts
@@ -0,0 +1,5 @@
+import { CardPostType } from '@/entities/post/types';
+
+export default {
+    title: '«Танец Снежинок» - А. Филипенко ',
+} as CardPostType
