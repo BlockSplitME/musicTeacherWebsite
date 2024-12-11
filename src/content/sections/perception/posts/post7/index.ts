eb9850ee552cd0467fa5bed6058e1789559770e7
Евгений Белов <evgenii.belov@tages.ru>
2024-12-11 10:49:45 +0300

    Добавлен пост С.Прокофьев <Вальс> из балета <Золушка>, <Бой часов>

---
+++ src/content/sections/perception/posts/post7/index.ts
@@ -0,0 +1,6 @@
+import { CardPostType } from '@/entities/post/types';
+
+export default {
+    title: 'С.Прокофьев "Вальс" из балета "Золушка", "Бой часов"',
+    subtitle: 'Золушка танцует с принцем... Мелодия словно взлетает фейерверком, радостно-взволнованная...',
+} as CardPostType
