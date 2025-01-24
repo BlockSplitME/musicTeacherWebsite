7dc97f8e99dbb3d10f039e255c734f1c13beb7b5
Евгений Белов <evgenii.belov@tages.ru>
2025-01-24 18:27:25 +0300

    Добавлено видео - Венское настроение

---
+++ src/content/sections/gamesAtChildrenMusicInstruments/posts/post7/Post.vue
@@ -0,0 +1,11 @@
+<template>
+  <div  style="display: flex; justify-content: center; height: 63vh;">
+    <video id="vid" controls>
+      <source src="./video/Венское_настроение.mp4" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
---
+++ src/content/sections/gamesAtChildrenMusicInstruments/posts/post7/index.ts
@@ -0,0 +1,5 @@
+import { CardPostType } from '@/entities/post/types';
+
+export default {
+    title: 'Шумовой оркестр «Венское настроение»',
+} as CardPostType
