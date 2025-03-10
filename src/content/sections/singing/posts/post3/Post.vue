1078bce9597759a4cc665f6646245c533b458f2f
Евгений Белов <evgenii.belov@tages.ru>
2025-03-10 10:25:44 +0300

    Добавлена песня И.Фролова - 'Весенняя'

---
+++ src/content/sections/singing/posts/post3/Post.vue
@@ -0,0 +1,7 @@
+<template>
+    <ImageBlock>
+        <img alt="весенняя_фролова" src="./image/И_Фролова_Весенняя.jpg">
+    </ImageBlock>
+</template>
+<style scoped>
+</style>
---
+++ src/content/sections/singing/posts/post3/index.ts
@@ -0,0 +1,5 @@
+import { CardPostType } from '@/entities/post/types';
+
+export default {
+    title: '«Весенняя» - И. Фролова ',
+} as CardPostType
