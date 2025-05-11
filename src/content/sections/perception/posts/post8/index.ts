f2a9fa2a448e0e2d8dd8e6f15b7e3fa2a4019d69
Евгений Белов <evgenii.belov@tages.ru>
2025-05-11 20:54:52 +0300

    Добавлена музыка М.Глинка-«Жаворонок»

---
+++ src/content/sections/perception/posts/post8/index.ts
@@ -0,0 +1,6 @@
+import { CardPostType } from '@/entities/post/types';
+
+export default {
+    title: 'М.Глинка «Жаворонок» (обр. М. Балакирева).',
+    subtitle: 'М. Глинка написал вокальное произведение «Жаворонок» для голоса и фортепиано на стихи...',
+} as CardPostType
